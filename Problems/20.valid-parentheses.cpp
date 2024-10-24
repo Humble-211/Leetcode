8322e13124a1b610d51f1238150563d058048e29
djenakcr <aduc1211524@gmail.com>
2024-10-23 21:35:46 -0400

    fuck this parentheses

--- Problems/20.valid-parentheses.cpp
+++ Problems/20.valid-parentheses.cpp
@@ -15,64 +15,127 @@
  *
  * Given a string s containing just the characters '(', ')', '{', '}', '[' and
  * ']', determine if the input string is valid.
- * 
+ *
  * An input string is valid if:
- * 
- * 
+ *
+ *
  * Open brackets must be closed by the same type of brackets.
  * Open brackets must be closed in the correct order.
  * Every close bracket has a corresponding open bracket of the same type.
- * 
- * 
- * 
+ *
+ *
+ *
  * Example 1:
- * 
- * 
+ *
+ *
  * Input: s = "()"
- * 
+ *
  * Output: true
- * 
- * 
+ *
+ *
  * Example 2:
- * 
- * 
+ *
+ *
  * Input: s = "()[]{}"
- * 
+ *
  * Output: true
- * 
- * 
+ *
+ *
  * Example 3:
- * 
- * 
+ *
+ *
  * Input: s = "(]"
- * 
+ *
  * Output: false
- * 
- * 
+ *
+ *
  * Example 4:
- * 
- * 
+ *
+ *
  * Input: s = "([])"
- * 
+ *
  * Output: true
- * 
- * 
- * 
+ *
+ *
+ *
  * Constraints:
- * 
- * 
+ *
+ *
  * 1 <= s.length <= 10^4
  * s consists of parentheses only '()[]{}'.
- * 
- * 
+ *
+ *
  */
 
 // @lc code=start
-class Solution {
+class Solution
+{
 public:
-    bool isValid(string s) {
-        
+    bool isValid(string s)
+    {
+        bool ret = false;
+        /*
+        int a = 0, aa = 0, b = 0, bb = 0, c = 0, cc = 0;
+        for (int i = 0; i < s.length(); i++)
+        {
+            switch (s.at(i))
+            {
+            case '{':
+                a++;
+                break;
+            case '}':
+                aa++;
+                break;
+            case '[':
+                b++;
+                break;
+            case ']':
+                bb++;
+                break;
+            case '(':
+                c++;
+                break;
+            case ')':
+                cc++;
+                break;
+            }
+        }
+        if (a == aa && b == bb && c == cc)
+            ret = true;
+
+        */
+       // ^ this method wont work if s = "([)]"...
+       // isValid(s) should return false
+        if (s.length() <2)
+            return ret;
+        int mid = s.length()/2;
+
+       for (int i = mid; i >0; i--) {
+        switch (s.at(i))
+            {
+            case '{':
+                a++;
+                break;
+            case '}':
+                aa++;
+                break;
+            case '[':
+                b++;
+                break;
+            case ']':
+                bb++;
+                break;
+            case '(':
+                c++;
+                break;
+            case ')':
+                cc++;
+                break;
+            }
+
+
+       }
+        return ret;
     }
 };
 // @lc code=end
-
--- test.cpp
+++ test.cpp
@@ -1,41 +1,46 @@
 // inserting into a vector
 #include <iostream>
-#include <vector>
+#include <string>
 using namespace std;
 
-vector<int> plusOne(vector<int> &digits)
-{
-    vector<int> temp;
-    unsigned long long temp1 = 0;
 
-    for (int i = 0; i < digits.size(); i++)
-    {
-        temp1 = temp1 * 10 + digits[i];
-    }
-    temp1 += 1;
-    // temp1 = 124
-    cout<<"digits.size() is: " <<digits.size() << endl;
-    cout <<"temp1 is:  ";
-    cout << temp1 ;
-    cout << endl;
-    // insert to temp[0] with last digit of temp1
-    while (temp1 > 0)
-    {
-        temp.insert(temp.begin(), temp1 % 10);
-        temp1 = temp1 / 10;
-    }
+bool val(string s) {
 
-    return temp;
+    bool ret = false;
+    int a = 0, aa = 0, b = 0, bb = 0, c = 0, cc = 0;
+    for (int i = 0 ; i < s.length(); i++) {
+        switch (i) {
+            case '{':
+                a++;
+                break;
+            case '}':
+                aa++;
+                break;
+            case '[':
+                b++;
+                break;
+            case ']':
+                bb++;
+                break;
+            case '(':
+                c++;
+                break;
+            case ')':
+                cc++;
+                break;
+        }   
+    }
+    if (a==aa && b==bb && c==cc)
+        ret = true;
+    return ret;
 }
-
 int main()
 {
-    vector<int> myvector{7,2,8,5,0,9,1,2,9,5,3,6,6,7,3,2,8,4,3,7,9,5,7,7,4,7,4,9,4,7,0,1,1,1,7,4,0,0,6};
+    string s = "(){}[]";
 
-    vector<int> temp = plusOne(myvector);
-    cout<<"temp is: ";
-    for (int i = 0 ; i <temp.size();i++){
-        cout << temp[i] << " ";
-    }
+    if (val(s))
+        cout << "true";
+    else 
+        cout << "false";
     return 0;
 }
